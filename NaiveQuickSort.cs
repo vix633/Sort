4b5067a5f9c3611bacae655ac8970e76750831bb
Viktor Yakovlyev <Viktor.Y@D2L.com>
2017-06-29 10:57:08 -0400

    adding brutal quicksort implementation

---
+++ NaiveQuickSort.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+namespace Sort
+{
+    //Super gross, not a practical solution, only a naive way to understand the algorithm
+    public class NaiveQuickSort : ISorter
+    {
+        public int[] Sort(int[] input){
+            if(input.Length < 2){
+				return input;
+			}
+            int pivot = input[0];
+            int[] high = input.Skip(1).Where(x => x > pivot).ToArray();
+            int[] low = input.Skip(1).Where(x => x <= pivot).ToArray();
+            return Sort(low).Concat(input.Take(1)).Concat(Sort(high)).ToArray();
+		}
+    }
+}
---
+++ QuickSort.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Sort
+{
+    public class QuickSort
+    {
+        public QuickSort()
+        {
+        }
+    }
+}
